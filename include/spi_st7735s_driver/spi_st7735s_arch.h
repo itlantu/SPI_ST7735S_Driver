2af2ea3235199caf7ae1264e629249d10d37e364
itlantu <it_lantu@outlook.com>
2025-12-07 00:15:17 +0800

    refactor: 修改了可移植函数的声明, 分为GPIO通讯函数和SPI通讯函数

--- include/spi_st7735s_driver/spi_st7735s_arch.h
+++ include/spi_st7735s_driver/spi_st7735s_arch.h
@@ -3,14 +3,21 @@
 
 #include <stdint.h>
 
-void st7735s_arch_delay_120ms();
+/* 延迟函数 */
+void st7735s_arch_delay_ms(uint16_t delay_ms);
 
-// 发送指令操作码
-uint8_t st7735s_arch_send_opcode(uint8_t opcode);
+/* SPI通讯 */
 // 发送数据
-uint8_t st7735s_arch_send_data(const uint8_t *data, uint16_t data_length);
+uint8_t st7735s_arch_spi_send(const uint8_t *data, uint16_t data_length);
 // 接收数据
-uint8_t st7735s_arch_receive_data(uint8_t *buffer,  uint16_t data_length, uint16_t buffer_length);
+uint8_t st7735s_arch_spi_receive(uint8_t *buffer,  uint16_t data_length, uint16_t buffer_length);
 
+/* GPIO通讯 */
+// RST硬件复位引脚
+void st7735s_arch_gpio_write_RST(uint8_t value);
+// DC引脚, 为高时表示传递的是数据信息, 为低时表示传递的是指令
+void st7735s_arch_gpio_write_DC(uint8_t value);
+// CS片选引脚
+void st7735s_arch_gpio_write_CS(uint8_t value);
 
 #endif // SPI_ST7735S_ARCH_H
